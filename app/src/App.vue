192fba2f19ae5ef7a897d79bccbda6ef4261eec1
otostogan <otostogan2020@gmail.com>
2024-01-21 18:31:32 +0200

    Update deployment script and message in App.vue

--- app/src/App.vue
+++ app/src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from "./components/HelloWorld.vue";
 			<img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
 		</a>
 	</div>
-	<HelloWorld msg="Hello from ci-cd zaebal zaebal" />
+	<HelloWorld msg="Hello from ci-cd zaebal zaebal zaebal" />
 </template>
 
 <style scoped>
